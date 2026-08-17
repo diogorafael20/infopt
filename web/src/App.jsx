8cd2adfc7eba3e22e5badce4e35605897e36aea8
Diogo Rafael Silva <diogorafael212@gmail.com>
2026-08-17 19:06:36 +0100

    Rebuild

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
-const API_URL = 'https://infopt.diogorafael212.workers.dev' // News aggregator API
+const API_URL = 'https://infopt.diogorafael212.workers.dev'
 
 export default function App() {
   const [categoria, setCategoria] = useState('portugal')
